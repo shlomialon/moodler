f702de2d1fd0e5a79310b4f2cd90854abd8c7687
Elad Keyshawn <elad.kishon@msmail.ariel.ac.il>
2017-03-23 19:17:17 +0200

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var checkPageButton = document.getElementById('hide_course_btn');
-    checkPageButton.addEventListener('click', function () {
-        console.log('hide btn clicked!');
-        chrome.tabs.getSelected(null, function (tab) {
-           
-        });
-    }, false);
-}, false);
