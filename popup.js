94f20f94de84d83c75971dcb0acb2c6d4d47a8e8
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-03-24 17:16:11 +0300

    better ui

--- popup.js
+++ popup.js
@@ -1,11 +1,129 @@
 
 $(document).ready(function () {
-    $('#submit_btn').click(onAddBtnClick);
-    updateCourseList();
     setUpLinkListeners();
+    initEverything();
 })
 
 
+function initEverything() {
+    //saves an item to localStorage
+    var saveReminder = function (id) {
+        var courselist = [];
+
+        chrome.storage.sync.get('courselist', function (result) {
+            if (result.courselist == null) {
+                courselist.push(id);
+                chrome.storage.sync.set({ 'courselist': courselist }, function () {
+                    console.log('saved');
+                    loadReminders();
+                });
+            } else {
+                courselist = result.courselist;
+                if (courselist.indexOf(id) == -1) {
+                    courselist.push(id);
+                }
+                chrome.storage.sync.set({ "courselist": courselist }, function () {
+                    console.log('saved');
+
+                    loadReminders();
+                });
+            }
+        });
+
+
+    };
+
+    //removes item from localStorage
+    var deleteReminder = function (id,content) {
+        var item = $('#' + id);
+        item.addClass('removed-item')
+            .one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function (e) {
+                $(this).remove();
+            });
+        var courselist = [];
+        chrome.storage.sync.get('courselist', function (result) {
+            if (result.courselist != null) {
+                courselist = result.courselist;
+                var index = courselist.indexOf(content);
+                if (index > -1) {
+                    courselist.splice(index, 1);
+                }
+
+                chrome.storage.sync.set({ 'courselist': courselist }, function () {
+                    console.debug('removed');
+                    loadReminders();
+                });
+            }
+        });
+
+    };
+
+
+    var createReminder = function (id,content) {
+        var courseItem = '<li id="' + id + '">' + content + '</li>',
+            list = $('.reminders li');
+
+
+        if (!$('#' + id).length) {
+
+            courseItem = $(courseItem).addClass('new-item');
+            $('.reminders').append(courseItem);
+
+
+            var createdItem = $('#' + id);
+
+            createdItem.append($('<button />', {
+                "class": "icon-trash delete-button",
+                "contenteditable": "false",
+                click: function () {
+                    deleteReminder(id,content);
+                }
+            }));
+
+            createdItem.on('keydown', function (ev) {
+                if (ev.keyCode === 13) return false;
+            });
+
+            saveReminder(content);
+        }
+    };
+
+    //handler for input
+    var handleInput = function () {
+        $('#input-form').on('submit', function (event) {
+            var input = $('#text');
+            var courseName = input.val();
+            event.preventDefault();
+            if (courseName) {
+                createReminder(courseName.replace(/\s/g,''),courseName);
+                input.val('');
+            }
+        });
+    };
+
+    var loadReminders = function () {
+        chrome.storage.sync.get('courselist', function (result) {
+            var items = result.courselist;
+            console.debug(items);
+            for (var item in items) {
+                createReminder(items[item].replace(/\s/g,''), items[item]);
+            }
+        });
+    };
+
+
+
+
+
+
+    var init = function () {
+        $('#text').focus();
+        loadReminders();
+        handleInput();
+    };
+    //start all
+    init();
+}
 
 
 function onAddBtnClick(params) {
@@ -61,11 +179,16 @@ function setUpLinkListeners() {
             var ln = links[i];
             var location = ln.href;
             ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
+                chrome.tabs.create({ active: true, url: location });
             };
         })();
     }
 }
+
+
+/*  logic for adding/removing items */
+
+
 
 
 
