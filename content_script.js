8b6ef57451473da39ac903e92edd148fa1ea4606
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-04-29 16:33:51 +0300

    basic auto login clicker

--- content_script.js
+++ content_script.js
@@ -1,58 +1,81 @@
 
 $(document).ready(function () {
 
+    var curr_url_path = window.location.href;
+    curr_url_path = curr_url_path.replace("https://moodlearn.ariel.ac.il/", "");
+    curr_url_path = curr_url_path.replace("http://moodlearn.ariel.ac.il/", "");
 
-    //remove annoying header
+    console.log(curr_url_path);
 
-    $('.container-fluid')[0].remove();
-    $('#frontpage-ariel').remove();
+    var signInLink = $('a').filter(function (index) { return $(this).text().includes('התחברות'); });
 
-    //remove accesibility
-    $('#inst339474').remove();
-    $('#inst28797').remove();
-    $('#inst4').remove();
-    $('#inst227770').remove();
-    $('#block-region-side-post').remove();
-    $('#block-region-side-pre').remove();
 
-    // links to remove
-    var links = ['דילוג על הקורסים שלי'];
-    links.forEach(function (linkName) {
-        var allFitLinks = $('a').filter(function (index) { return $(this).text().includes(linkName); });
-        for (var i = 0; i < allFitLinks.length; i++) {
-            allFitLinks[i].remove();
+    switch (curr_url_path) {
 
+        case "": {
+            if (signInLink.length > 0) {
+                signInLink[0].click();
+            } else {
+                removeMainFrameAndCourses();
+            }
+            break;
         }
 
-    }, this);
+        case "login/index.php": {
+            $('#username').val("315631887");
+            $('#password').val("9885");
+            $('#loginbtn').click();
+        }
+
+        default:
+            break;
+
+    }
 
 
 
-    // var coursesNames = ['מתמטיקה בדידה', 'תקשורת ומחשוב', 'סוגיות בכלכלה בראי ההלכה',
-    //     'תורת המספרים', 'מסדי נתונים', 'מחשבת חז"ל - התפתחות והתמודדות', 'מחשבת התפילה',
-    //     'מנהגי ישראל', 'מבוא לתכנות מונחה עצמים', 'מבוא לעולם סייבר', 'מבוא למחשבים ושפת C', 'מבוא לחישוב',
-    //     'לוגיקה ותורת הקבוצות', 'אלגוריתמים 1', 'אוטומטים ושפות פורמליות 1', 'אנגלית טכנית', 'ארכיטקטורה של מחשבים'
-    //     , 'הסתברות'
-    // ];
+  function removeMainFrameAndCourses() {
+        //remove annoying header
+        $('.container-fluid')[0].remove();
+        $('#frontpage-ariel').remove();
 
-    var courseNames = [];
-    chrome.storage.sync.get('courselist', function (result) {
-         courseNames = result.courselist;
-        console.debug(courseNames);
-        // remove courses
-        courseNames.forEach(function (coursesName) {
-            var allFitLinks = $('a').filter(function (index) { return $(this).text().includes(coursesName); });
+        //remove accesibility
+        $('#inst339474').remove();
+        $('#inst28797').remove();
+        $('#inst4').remove();
+        $('#inst227770').remove();
+        $('#block-region-side-post').remove();
+        $('#block-region-side-pre').remove();
+
+        // links to remove
+        var links = ['דילוג על הקורסים שלי'];
+        links.forEach(function (linkName) {
+            var allFitLinks = $('a').filter(function (index) { return $(this).text().includes(linkName); });
             for (var i = 0; i < allFitLinks.length; i++) {
-                var courseBox = allFitLinks[i].closest('.coursebox');
-                if (courseBox != null) {
-                    courseBox.remove();
-                }
+                allFitLinks[i].remove();
+
             }
 
         }, this);
-    });
 
 
+        var courseNames = [];
+        chrome.storage.sync.get('courselist', function (result) {
+            courseNames = result.courselist;
+            console.debug(courseNames);
+            // remove courses
+            courseNames.forEach(function (coursesName) {
+                var allFitLinks = $('a').filter(function (index) { return $(this).text().includes(coursesName); });
+                for (var i = 0; i < allFitLinks.length; i++) {
+                    var courseBox = allFitLinks[i].closest('.coursebox');
+                    if (courseBox != null) {
+                        courseBox.remove();
+                    }
+                }
+
+            }, this);
+        });
+    }
 
 
 
