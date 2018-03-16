daaff6d01842344a44086c03224c606d2e2300db
shlomialon <shlomi.al90@gmail.com>
2018-03-16 14:46:26 +0200

    redesign, removed css file and implementation skeleton css

--- app/src/components/main.js
+++ app/src/components/main.js
@@ -1,32 +1,39 @@
 import {h} from "hyperapp"
 
 export default ({loading, courseList, typeBox}, {init, setTypeBoxValue, saveCourse, deleteCourse}) =>
-  <div class="reminder-container" oncreate={init}>
+    <div class="reminder-container" oncreate={init}>
+        <div className={"row"}>
+            <center>
+                <input oncreate={element => element.focus()} oninput={e => setTypeBoxValue(e.target.value)} type="text"
+                       placeholder="הקלד שם הקורס.." value={typeBox}
+                       onkeyup={e => {
+                           e.keyCode === 13 ? saveCourse() : null
+                       }}/>
 
-      <input oncreate={element => element.focus()} oninput={e => setTypeBoxValue(e.target.value)} type="text"
-             placeholder="Course name..." value={typeBox}
-             onkeyup={e => {e.keyCode === 13 ? saveCourse() : null}}/>
-      <input type="submit" value="Add" onclick={saveCourse}>Add</input>
+                <input value={"הוספה"} type="submit" onclick={saveCourse} className={"button-primary"}>
+                </input>
+            </center>
+        </div>
+        <div class="container">
+            <ul>{
+                courseList.map((course, idx) => {
+                    return <div class="row" id={idx}>
+                        <div class="one-third column">
+                            <button onclick={deleteCourse.bind(null, idx)}
+                                    class="icon-trash delete-button">הסר
+                            </button>
+                        </div>
+                        <div class="two-thirds column">{course}</div>
 
-      <div class="container">
-          <ul class="reminders">
-
-              {
-                  courseList.map((course, idx) => {
-                      return <li id={idx}> {course}
-
-                          <button onclick={deleteCourse.bind(null, idx)}
-                                  class="icon-trash delete-button"/>
-                      </li>
-                  })
-              }
-
-          </ul>
-          {
-              !courseList.length ? <h3>No courses added yet.</h3> : null
-          }
-
-      </div>
-  </div>;
+                    </div>
+                })
+            }
+                {
+                    !courseList.length ? <h3>No courses added yet.</h3> : null
+                }
+            </ul>
+        </div>
+    </div>
+;
 
 
